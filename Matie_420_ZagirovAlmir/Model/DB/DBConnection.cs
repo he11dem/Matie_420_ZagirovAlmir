761afea76ffe7302a33ea54ddccbd934c73f611a
202106 <202106@1305-015>
2025-04-19 10:05:06 +0300

    Добавьте файлы проекта.

---
+++ Matie_420_ZagirovAlmir/Model/DB/DBConnection.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Matie_420_ZagirovAlmir.Model.DB
+{
+    internal class DBConnection
+    {
+        public static Matie_420_ZagirovAlmirEntities matie = new Matie_420_ZagirovAlmirEntities();
+    }
+}
